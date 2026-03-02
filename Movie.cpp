39e8d999c1355eee35f9fd8ed18afd62f357d934
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-03-01 19:49:06 -0800

    update+done

--- Movie.cpp
+++ Movie.cpp
@@ -13,18 +13,18 @@ private:
     int year;
 public:
     //setter and getter methods
-    void setScreen(string screen)           {screenWrite = screen;}
-    void setTitle(string ttl)               {title = ttl;}
-    void setYear(int yr)                    {year = yr;}
-    string getScreen()                      {return screenWrite;}
-    string getTitle()                       {return title;}
-    int getYear()                           {return year;}
+    void setScreen(string screen)      {screenWrite = screen;}
+    void setTitle(string ttl)          {title = ttl;}
+    void setYear(int yr)               {year = yr;}
+    string getScreen()                 {return screenWrite;}
+    string getTitle()                  {return title;}
+    int getYear()                      {return year;}
 
     //print method
     void print(){
         cout << "Movie: " << title << endl;
         cout << setw(W10) << "Year released: " << year << endl;
-        cout << setw(W10) << "ScreenWriter" << screenWrite << endl;
+        cout << setw(W10) << "ScreenWriter: " << screenWrite << endl;
     }
 };
 
@@ -42,8 +42,21 @@ int main(){
             fin >> yr;
             fin >> scr;
             Movie tmp; //temporary object
-            
+            tmp.setTitle(ttl);
+            tmp.setYear(yr);
+            tmp.setScreen(scr);
+            movies.push_back(tmp);
+            fin.ignore(10000, '\n');
         }
     }
+    else
+        cout << "Input file not found.\n";
+
+    //outputting vector data
+    for (auto val: movies){
+        val.print();
+        cout << endl;
+    }
+
     return 0;
 }
